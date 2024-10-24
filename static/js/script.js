110972fc02da179cce5b33e1cce995a9842ce2a6
matheus7git <matheus07gitteles@gmail.com>
2024-10-24 12:23:58 -0300

    aula 24/10

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+// função para mostrar o spinner enquanto o formulário está sendo enviado
+document.getElementById('decode-form').addEventListener('submit',
+    function() {
+        // Mostrar o spinner
+        document.getElementById('loading-spinner').style,display = 'block';
+
+        // Esconder Resultado
+        document.getElementById('result').style.display = 'none';
+        
+
+    }
+)
